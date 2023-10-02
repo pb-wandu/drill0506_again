595ccf7c8f6863e472d4ca15e4294d4e635cf82f
Cho Seung Wan <csw4412@gmail.com>
2023-10-02 15:31:00 +0900

    반복적으로 만들어지는 랜덤 손 화살표 위치로 캐릭터 이동 구현 완료 (t값이 1을 넘으면 플레이어 위치를 목적지 위치로 고정한 뒤 새로운 화살표 생성)

--- drill05.py
+++ drill05.py
@@ -36,16 +36,21 @@ def reset_world():
     frame = 0
     action = 3
 
-    sx, sy = cx, cy # p1 : 시작점
+    set_new_target_arrow()
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
+    sx, sy = cx, cy  # p1 : 시작점
     # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
-    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT) # p2 : 끝점
+    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2 : 끝점
     t = 0.0
+
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     arrow.draw(hx, hy)
-    character.clip_draw(frame * 100, 100 * actionq, 100, 100, cx, cy)
+    character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
 def update_world():
@@ -61,6 +66,9 @@ def update_world():
         cx = (1 - t) * sx + t * hx # cx는 시작 x와 끝 x를 1-t : t의 비율로 섞은 위치
         cy = (1 - t) * sy + t * hy # cy는 시작 y와 끝 y를 1-t : t의 비율로 섞은 위치
         t += 0.001
+    else:
+        cx, cy = hx, hy # 캐릭터 위치를 목적지 위치와 강제로 정확히 일치시킴
+        set_new_target_arrow()
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 hide_cursor()
